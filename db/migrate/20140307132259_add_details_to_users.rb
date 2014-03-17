0d65a1c627b48776ab44970dbb42f502e9616f1f
Kelly <wang.chihjau@gmail.com>
2014-03-17 23:43:27 +0700

    reset db 2

--- db/migrate/20140307132259_add_details_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :faculty, :string
-    add_column :users, :major, :string
-    add_column :users, :description, :text
-  end
-end
